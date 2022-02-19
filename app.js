3fa397042cf5397c9f5f17e3db1a2c00257303b9
EniMete2 <91092849+EniMete2@users.noreply.github.com>
2022-02-19 16:08:12 -0500

    Added Notification Animation

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const tl = gsap.timeline({
 });
 
 const home = document.querySelector(".home");
+const notifications = document.querySelector(".notifications");
 
 gsap.set(".feather", { scale: 0 });
+
 home.addEventListener("click", () => {
   gsap.fromTo(".home-svg", { scale: 1 }, { scale: 0.9, yoyo: true, repeat: 1 });
   gsap.fromTo(
@@ -13,4 +15,26 @@ home.addEventListener("click", () => {
     { y: 20, scale: 1.5, duration: 1, stagger: 0.2 }
   );
   gsap.fromTo(".right-feather", { x: 0 }, { x: 5 });
+});
+
+gsap.set(".bell", { transformOrigin: "top center" });
+gsap.set(".ringer", { transformOrigin: "top center" });
+gsap.set(".wave", { opacity: 0, transformOrigin: "bottom" });
+
+notifications.addEventListener("click", () => {
+  gsap.fromTo(
+    ".bell",
+    { rotation: -5 },
+    { rotation: 0, ease: "elastic.out(5, 0.2)", duration: 2 }
+  );
+  gsap.fromTo(
+    ".ringer",
+    { rotation: -5, x: 0.5 },
+    { rotation: 0, x: 0, ease: "elastic.out(5, 0.2)", duration: 2 }
+  );
+  gsap.fromTo(
+    ".wave",
+    { scale: 0, opacity: 1 },
+    { scale: 1.3, opacity: 0, duration: 1 }
+  );
 });
